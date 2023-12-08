cabccb074a7f8f5ece036ce015a7d887f7e34055
mvangg <marivang@live.com>
2023-12-08 14:20:58 -0800

    front end structure part one

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../Untitled design (1).png'
+
+const Header = () => {
+    return (
+        <div className="header-image">
+            <div className='header-container'>
+                <h1>
+                    <img src={logo}/>
+                    Dog Tinder
+                </h1>
+            </div>
+        </div>
+    )
+}
+export default Header
+// "/Users/learnacademy/Desktop/cat-tinder-frontend/src/assets/Untitled design (1).png"
---
+++ src/pages/CatEdit.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CatEdit = () => {
+    return (
+        <>
+        CatEdit
+        </>
+    )
+}
+export default CatEdit
