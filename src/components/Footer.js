ea5599d30b1472e33080343b001aec3c73db28da
LEARN Academy <learnacademy@MacBook-Air-6.local>
2023-12-14 15:54:24 -0800

    added to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Footer = () => {
     return (
         
-        <footer className='footer'>ⓒ M and Dalton's | Dog Tinder</footer>
+        <footer className='footer' >ⓒ 2023 Dog Tinder{" | "}
+        <a href='https://www.linkedin.com/in/dalton-mcauliffe/' style={{ color: "white" }}>Dalton's LinkedIn</a>{" | "}
+        <a href='https://www.linkedin.com/in/mvanggg/' style={{ color: "white" }}>M's LinkedIn</a>
+        </footer>
+
         
     )
 }
